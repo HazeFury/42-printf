09dd751bea3cfef220d1293453df5012f6e22eb2
Marc-Antoine Berger <marberge@z3r9p2.42lyon.fr>
2025-11-25 18:16:08 +0100

    init

--- ft_printf.c
+++ ft_printf.c
@@ -6,16 +6,33 @@
 /*   By: marberge <marberge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/25 12:33:56 by marberge          #+#    #+#             */
-/*   Updated: 2025/11/25 12:55:22 by marberge         ###   ########.fr       */
+/*   Updated: 2025/11/25 18:15:09 by marberge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdarg.h>
 
-int	ft_printf(const char *str, ...);
+int	ft_printf(const char *str, ...)
+{
+	va_list	args;
+	int	char_count;
+	char	buffer[1024];
+	int	i;
 
-// int	main(void)
-// {
-// 	printf("hello world");
-// 	return (0);
-// }
+	i = 0;
+	while (str[i])
+	{
+
+	}
+	va_start(args, str);
+	printf("%d - %c - %d\n", va_arg(args, int), va_arg(args, int), va_arg(args, int));
+	va_end(args);
+	return (0);
+}
+
+int	main(void)
+{
+	ft_printf("toto", 2, 'g', 30);
+	return (0);
+}
